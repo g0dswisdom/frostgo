cdda1b620bd06e7adf0c5bd8628d551473169658
kf <scaryritualz@gmail.com>
2023-12-27 22:13:40 +0200

    Removed unused function

--- util.go
+++ util.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func customRequest(b *Bot, method, endpoint string, data map[string]interface{}, headers map[string]interface{}) *http.Response {
@@ -18,22 +17,4 @@ func customRequest(b *Bot, method, endpoint string, data map[string]interface{},
 
 func decode(resp *http.Response, target interface{}) {
 	json.NewDecoder(resp.Body).Decode(target)
-}
-
-func ISO8601(timestamp string, daysToAdd, minutesToAdd int) string {
-	parsedTime, err := time.Parse(time.RFC3339, timestamp)
-	if err != nil {
-		return ""
-	}
-
-	newTime := parsedTime
-	if daysToAdd != 0 {
-		newTime = newTime.AddDate(0, 0, daysToAdd)
-	}
-
-	if minutesToAdd != 0 {
-		newTime = newTime.Add(time.Minute * time.Duration(minutesToAdd))
-	}
-
-	return newTime.Format(time.RFC3339)
 }
